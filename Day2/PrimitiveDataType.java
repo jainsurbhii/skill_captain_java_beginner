8e9765fc643aaff9b148876b36c80d22b08b2e23
surbhi Jain <47022009+jainsurbhii@users.noreply.github.com>
2023-07-08 15:20:08 +0530

    PrimitiveDataType

---
+++ Day2/PrimitiveDataType.java
@@ -0,0 +1,22 @@
+public class PrimitiveDataType {
+
+    public static void main(String[] args) {
+        byte abyte=25;
+        short bshort=1234;
+        int cint=2898;
+        long dlong=6365364;
+        float efloat=7.2f;//6 decimal digit
+        double fdouble=45.878;// 15 decimal digit
+        char gchar='a';
+        boolean hboolean=false;
+        System.out.println(abyte);
+        System.out.println(bshort);
+        System.out.println(cint);
+        System.out.println(dlong);
+        System.out.println(efloat);
+        System.out.println(fdouble);
+        System.out.println(gchar);
+        System.out.println(hboolean);
+
+    }
+}
